0da73ea289f4dd89a7d9b883bb369aa4f1206881
杂草 <hatanaka.keika.88r@st.kyoto-u.ac.jp>
2020-07-07 10:14:35 +0900

    Update Bongo_Fethcer.py

--- Bongo_Fethcer.py
+++ Bongo_Fethcer.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 
 import urllib.request as req
+import urllib.parse as ps
 from bs4 import BeautifulSoup
 
 def AquireURL():
     Base_URL='https://www.kyoto-np.co.jp/category/bongo'
-    Kyoto_Base_URL='https://www.kyoto-np.co.jp'
 
     with req.urlopen(Base_URL) as r:
         b = r.read()
@@ -16,7 +16,7 @@ def AquireURL():
 
     Top_Artcle = soup.select_one('#contents > main > div > div:nth-child(3) > article:nth-child(1) > a')
 
-    return Kyoto_Base_URL + Top_Artcle.attrs['href']
+    return ps.urljoin(Kyoto_Base_URL + Top_Artcle.attrs['href'])
 
 if __name__ == "__main__":
 
